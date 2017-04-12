5967587d0440f575d567ebec576c317350810aea
Muganda Dizo <mugandadizo@gmail.com>
2017-04-12 06:58:28 +0000

    Refactered todos controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,8 @@
 class TodosController < ApplicationController
+    
+    before_action :set_todo, only: [:show, :edit, :update, :destroy]
+    
+    
     def new
         @todo = Todo.new
     end
@@ -13,15 +17,15 @@ class TodosController < ApplicationController
     end
     end
     def show
-        @todo = Todo.find(params[:id])
+        
     end
     
     def edit
-        @todo = Todo.find(params[:id])
+        
     end
     
     def update
-        @todo = Todo.find(params[:id])
+        
         if @todo.update(todo_params)
             flash[:notice] = "Todo was updated"
             redirect_to todo_path(@todo)
@@ -33,12 +37,15 @@ class TodosController < ApplicationController
         @todos = Todo.all
     end
     def destroy
-        @todo = Todo.find(params[:id])
+       
         @todo.destroy
         flash[:notice] = "Todo was deleted"
         redirect_to todos_path
     end
     private
+        def set_todo
+            @todo = Todo.find(params[:id])
+        end
         def todo_params
             params.require(:todo).permit(:name, :description)
         end
